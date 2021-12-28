45d53a9faa57f48790b53de5b5b65c8b2a4c6a94
andrea manfroi <andreamanfroi@libero.it>
2021-12-28 10:53:20 +0000

    Adding new solutions with comments.

--- lists/filtering/lastButElement.hs
+++ lists/filtering/lastButElement.hs
@@ -1,9 +1,48 @@
-myButLast :: [a] -> a
-myButLast [] = error "No but to end for empty lists!"
-myButLast [x] = error "Just 1 element!"
-myButLast [x1,x2] = x1
-myButLast (_:x1:xs) = myButLast (x1:xs)
+-- PROBLEM:
+-- Find the last but one element of a list, such as that
+-- lastButOne [1,2 3, 4, 5] is 4
+-- lastButOne "hello mate" is t
+
+--------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------
+
+-- 1. Declarative way.
+--    When the list is empty, then we can return an error,
+--    as well as when the list contains only one element.
+--    In the base case of two elements, we grab the first in order.
+--    When more than two elements, we can recursively call the function on the last two elements,
+--    disregarding whatever comes first in the list.
+lastButOne :: [a] -> a
+lastButOne [] = error "No but to end for empty lists!"
+lastButOne [x] = error "Just 1 element!"
+lastButOne [x1,x2] = x1
+lastButOne (_:x1:xs) = lastButOne (x1:xs)
+
+-- 2. Using the combination of last and init.
+--    This solution is quite unsafe as per documentation:
+--    init: Return all the elements of a list except the last one.
+--          The list must be non-empty.
+lastButOne' :: [a] -> a
+lastButOne' = last . init
+
+-- 3. Reversing the list and getting the head of the tail :)
+--    [1,2,3,4] -> reverse [1,2,3,4] -> [4,3,2,1]
+--    tail [4,3,2,1] -> [3,2,1]
+--    head [3,2,1] -> 3
+lastButOne'' :: [a] -> a
+lastButOne'' = head . tail . reverse
+
+-- 4. Using list indexing
+lastButOne''' :: [a] -> a
+lastButOne''' xs = (reverse xs) !! 1
 
 main = do
        putStrLn "The but last element of [1..10] is:"
-       print (myButLast [1..10])
+       print $ lastButOne [1..10]
+       print $ lastButOne "hello mate"
+       print $ lastButOne' [1..10]
+       print $ lastButOne' "hello mate"
+       print $ lastButOne'' [1..10]
+       print $ lastButOne'' "hello mate"
+       print $ lastButOne''' [1..10]
+       print $ lastButOne''' "hello mate"
--- lists/filtering/lastElement.hs
+++ lists/filtering/lastElement.hs
@@ -1,8 +1,49 @@
+-- PROBLEM:
+-- Find the last element of a list, such as that
+-- lastFunction [1,2 3, 4, 5] is 5
+-- lastFunction "hello mates" is s
+
+--------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------
+
+
+import Data.List
+
+-- 1. Recursive solution.
+--    When the list is empty, then we can return an error.
+--    When there is only one element, then we return the element itself.
+--    When we have more than one element, we recursively call the myLast function on the tail.
 myLast :: [a] -> a
 myLast [] = error "No end for empty lists!"
 myLast [x] = x
 myLast (_:xs) = myLast xs
 
+-- 2. Using foldr1 from Data.List and the const function on the identity function
+myLast' :: [a] -> a
+myLast' = foldr1 (const id)
+
+-- 3. Using foldr1 from Data.List and the flip function on the const function
+myLast'' :: [a] -> a
+myLast'' = foldr1 (flip const)
+
+-- 4. Combining a call to head of the reversed list!
+myLast''' :: [a] -> a
+myLast''' xs = head (reverse xs)
+-- can be equivalently written as myLast''' = head . reverse
+
+-- 5. Mmmmmhhhh.....
+myLast'''' :: [a] -> a
+myLast'''' = foldl1 (curry snd)
+
+-- 6. Using built-in 'last' function from Prelude
+myLast''''' :: [a] -> a
+myLast''''' = last
+
 main = do
        putStrLn "The last element of [1..10] is:"
        print (myLast [1..10])
+       print (myLast' [1..10])
+       print (myLast'' [1..10])
+       print (myLast''' [1..10])
+       print (myLast'''' [1..10])
+       print (myLast''''' [1..10])
