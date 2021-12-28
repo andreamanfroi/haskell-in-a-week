2279d0d8bbecdd79189e4c2d12cac72fdc256e33
andrea manfroi <andreamanfroi@libero.it>
2021-12-28 22:06:15 +0000

    palindrome.hs, reverse and nth element examples

--- lists/sorting/palindrome.hs
+++ lists/sorting/palindrome.hs
@@ -1,9 +1,27 @@
+-- PROBLEM:
+-- Check if a prompted word is palindrome, i.e. can be read equivalently from left to right
+-- and right to left, such as:
+-- palindrome "kayak" = True
+-- palindrome "hello" = False
+
+--------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------
+
 import Data.List
 
+-- 1. Checking the reverse is the same as the original
 isPalindrome :: Eq a => [a] -> Bool
 isPalindrome [] = False
 isPalindrome [x] = True
 isPalindrome word = (word == reverse word)
+
+-- 2. Checking the half string is the same as the half reversed
+isPalindrome' :: Eq a => [a] -> Bool
+isPalindrome' list = take halfLen list == reverse (drop ( halfLen + (len `mod` 2)) list)
+                    where
+                    len = length list
+                    halfLen = len `div` 2
+
 
 
 main = do
@@ -11,3 +29,4 @@ main = do
        putStrLn "Please, insert a word..."
        word <- getLine
        print $ isPalindrome word
+       print $ isPalindrome' word
--- lists/sorting/reversing.hs
+++ lists/sorting/reversing.hs
@@ -1,6 +1,40 @@
+-- PROBLEM:
+-- Reverse the order of a list of elements, such as that
+-- myReverse [1,2,3,4] = [4,3,2,1]
+-- myReverse "hello" = "olleh"
+
+--------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------
+
+
 import Data.List
+
+-- 1. Recursive
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse [x] = [x]
+myReverse [x1,x2] = [x2,x1]
+myReverse (x:xs) = myReverse xs ++ [x]
+
+-- 2. Computationally efficient
+myReverse' :: [a] -> [a]
+myReverse' list = myReverse'' list []
+  where
+    myReverse'' [] reversed     = reversed
+    myReverse'' (x:xs) reversed = myReverse'' xs (x:reversed)
+
+-- 3. A very concise method!
+myReverse''' :: [a] -> [a]
+myReverse''' xs = foldr (\x fId empty -> fId (x : empty)) id xs []
 
 main = do
        putStrLn "You can easily reverse list with the reverse function"
        let arr = ['h','e','l','l','o']
        print $ reverse arr
+       let arr = [1,2,3,4]
+       print $ reverse arr
+       putStrLn "Or revert using a recursive function"
+       let arr = ['h','e','l','l','o']
+       print $ myReverse' arr
+       let arr = [1,2,3,4]
+       print $ myReverse' arr
