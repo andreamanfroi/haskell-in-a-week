86c4df6f555799043e4f781b9d258bcbffcccbb5
andrea manfroi <andreamanfroi@libero.it>
2021-12-28 13:14:51 +0000

    Commented decisions.hs and delete simpleFilter.hs

--- lists/filtering/simpleFilter.hs
+++
@@ -1,7 +0,0 @@
-import Data.List
-main = do
-       let users = [("Saurabh", 35), ("John", 45), ("Doe", -5)]
-       putStrLn (show users)
-       case (find (\(_, age) -> age < 1 || age > 100) users) of
-        Nothing -> Right users
-        --Just (name, age) -> Left $ name <> " seems to have an incorrect age: " <> show age
