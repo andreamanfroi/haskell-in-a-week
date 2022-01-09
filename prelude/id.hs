adb7127df59bca7d30097b53a8eeafb3aac8f2b2
andrea manfroi <andreamanfroi@libero.it>
2022-01-09 22:47:18 +0000

    Added identity function example

---
+++ prelude/id.hs
@@ -0,0 +1,10 @@
+
+identity x = id x
+
+main = do
+  print $ "The value of the id function applied to the int 3 is :"
+  print $ identity 3
+  print $ "The value of the id function applied to the string hello  is :"
+  print $ identity "hello"
+  print $ "Yeah, you get you input back, also for list!"
+  print $ map identity [1,2,3,4]
