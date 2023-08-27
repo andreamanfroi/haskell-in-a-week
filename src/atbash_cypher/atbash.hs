00b5705eed567b9db46349495d251025c2cfe084
andreamanfroi <andreamanfroi@libero.it>
2023-08-27 09:44:30 +0100

    atbash''

--- src/atbash_cypher/atbash.hs
+++ src/atbash_cypher/atbash.hs
@@ -5,6 +5,7 @@
 -}
 
 import Data.Maybe (mapMaybe)
+import Data.Char (ord, chr)
 
 atbash:: String -> String
 atbash = mapMaybe (\c -> lookup c (zip ['a'..'z'] (reverse ['a'..'z'])))  
@@ -14,10 +15,18 @@ atbash' x = mapMaybe atb x
         where rev = (zip ['a'..'z'] (reverse ['a'..'z']))
               atb c = lookup c rev
 
+atbash'' :: String -> String
+atbash'' = map atb
+  where
+    atb c
+      | 'a' <= c && c <= 'z' = toEnum $ ord 'a' + (ord 'z' - ord c) `mod` 26
+      | otherwise = c
+
 main :: IO ()
 main = do
-    putStrLn "Let's atbash a string! Please, input any string here below:"
+    putStrLn "Let's atbash a string! Please, input any lowercase string here below:"
     x <- getLine
-    let y = atbash' x
+    let y = atbash x
     -- let y = atbash' x
+    -- let y = atbash'' x
     print y
