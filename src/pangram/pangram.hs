1947a3769059adff983044b54ca1efca806dc5b5
andreamanfroi <andreamanfroi@libero.it>
2024-03-30 11:58:33 +0000

    clean ups

--- src/pangram/pangram.hs
+++ src/pangram/pangram.hs
@@ -5,9 +5,3 @@ pangram x = all (`isPresentAtLeastOnce` x) ['a'..'z']
 
 isPresentAtLeastOnce :: Eq a => a -> [a] -> Bool
 isPresentAtLeastOnce x xs = x `elem` xs
-
--- main :: IO ()
--- main = do
---     let word = "the quick brown fox jumps over the lazy dog"
---     -- let word = "definitely not a pangram word"
---     print $ pangram word
